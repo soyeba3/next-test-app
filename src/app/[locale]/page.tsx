d8b8d49a3934d03a726ace8ae416b50d999a4456
soyeba3 <soyebchandani@gmail.com>
2024-05-24 11:21:10 +0600

    Removed page og

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,45 +8,17 @@ export default async function Home() {
   const t = await getTranslations("Home");
 
   return (
-    <>
-      <head>
-        <title>The Rock (1996)</title>
-        <meta property="og:title" content="Soyeb testing title in root" />
-        <meta
-          property="og:image"
-          content="https://support.rebrandly.com/hc/article_attachments/17557974325021"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://support.rebrandly.com/hc/article_attachments/17557974325021"
-        />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" />
-        <meta
-          property="og:image:alt"
-          content="A shiny red apple with a bite taken out"
-        />
-        <meta
-          name="twitter:image"
-          content="https://avatars.githubusercontent.com/u/88161150?v=4"
-        />
-        <meta name="twitter:image:type" content="image/jpeg" />
-        <meta name="twitter:image:width" content="400" />
-        <meta name="twitter:image:height" content="300" />
-      </head>
-      <div className="container">
-        <h1 className="p-4 text-lg font-bold text-center">Todos</h1>
-        <h1 className="p-4 text-lg font-bold text-center">{t("title")}</h1>
-        <ul className="flex flex-col gap-5 p-5">
-          {data.map((item) => (
-            <SingleTodo key={item.id} todo={item} />
-          ))}
-        </ul>
-        <div className="flex justify-center my-10">
-          <Button />
-        </div>
+    <div className="container">
+      <h1 className="p-4 text-lg font-bold text-center">Todos</h1>
+      <h1 className="p-4 text-lg font-bold text-center">{t("title")}</h1>
+      <ul className="flex flex-col gap-5 p-5">
+        {data.map((item) => (
+          <SingleTodo key={item.id} todo={item} />
+        ))}
+      </ul>
+      <div className="flex justify-center my-10">
+        <Button />
       </div>
-    </>
+    </div>
   );
 }
